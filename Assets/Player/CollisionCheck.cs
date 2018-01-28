600bc5bf571e6af26b1da9ca38d70235dfc809ef
Boothand <boothand@hotmail.com>
2018-01-28 14:46:11 +0100

    ALmost collisions. Obstacles yay

---
+++ Assets/Player/CollisionCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour
+{
+	Player playerInstance;
+
+	void Awake()
+	{
+		playerInstance = transform.parent.GetComponent<Player>();
+	}
+
+	void Start ()
+	{
+		
+	}
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		print("Collision");	
+		playerInstance.OnCollided(collider);
+		//Audio.PlayAudioSource(Audio.Instance.error);
+	}
+
+	void Update ()
+	{
+		
+	}
+}
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -88,6 +88,11 @@ public class Player : MonoBehaviour
 		}
 	}
 
+	public void OnCollided(Collider collider)
+	{
+
+	}
+
 	// Add a new target node to the end of the list. Don't need all the points before we start traveling.
 	private void Event_SpawnedNode(TargetNode inNode)
 	{
