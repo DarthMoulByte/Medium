1dc8337e2942b70581b98267d562f31b6f7afcd4
Bjørnar <bjornar24@gmail.com>
2018-01-28 11:25:23 +0100

    Audio manager from last year

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+	public static Audio Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = FindObjectOfType<Audio>();
+			}
+
+			return _instance;
+		}
+	}
+
+	private static Audio _instance;
+
+	public AudioSource travel;
+
+	public List<AudioSource> swoosh;
+	public List<AudioSource> error;
+
+	public static void PlayAudioSource(AudioSource audioSource, float fadeInTime = 0f)
+	{
+		if (audioSource != null && !audioSource.isPlaying)
+		{
+			if (fadeInTime == 0)
+			{
+				audioSource.Play();
+			}
+			else
+			{
+				_instance.StartCoroutine( _instance.FadeInAudioSource( audioSource , fadeInTime ) );
+			}
+		}
+	}
+
+	public static void PlayAudioSource(List<AudioSource> audioSourceList, float fadeInTime = 0f, float randomPitch = 0.1f)
+	{
+		var randomIndex = Random.Range(0, audioSourceList.Count);
+		var audioSource = audioSourceList[randomIndex];
+
+		audioSource.pitch += Random.Range(-randomPitch, randomPitch);
+
+		PlayAudioSource(audioSource, fadeInTime);
+	}
+
+	public static void StopAudioSource(AudioSource audioSource, float fadeOutTime = 0f)
+	{
+		if (audioSource != null)
+		{
+			if (fadeOutTime == 0)
+			{
+				audioSource.Stop();
+			}
+			else
+			{
+				_instance.StartCoroutine(_instance.FadeOutAudioSource(audioSource, fadeOutTime));
+			}
+		}
+	}
+
+	private IEnumerator FadeOutAudioSource(AudioSource audioSource, float time)
+	{
+		var originalVolume = audioSource.volume;
+
+		time = Mathf.Max(0.01f, time);
+		originalVolume = Mathf.Max(0.01f, originalVolume );
+
+		var ratio = originalVolume/time;
+
+		while (audioSource.volume >= 0f)
+		{
+			audioSource.volume -= ratio * Time.deltaTime;
+			if (audioSource.volume <= 0f) break;
+			yield return null;
+		}
+
+		audioSource.Stop();
+		audioSource.volume = originalVolume;
+
+		yield return null;
+	}
+
+	private IEnumerator FadeInAudioSource(AudioSource audioSource, float time)
+	{
+		var originalVolume = audioSource.volume;
+
+		audioSource.volume = 0f;
+		audioSource.Play();
+
+		time = Mathf.Max(0.01f, time);
+		originalVolume = Mathf.Max(0.01f, originalVolume );
+
+		var ratio = originalVolume/time;
+
+		while (audioSource.volume >= 0f)
+		{
+			audioSource.volume += ratio * Time.deltaTime;
+			if (audioSource.volume >= originalVolume) break;
+			yield return null;
+		}
+
+		audioSource.volume = originalVolume;
+
+		yield return null;
+	}
+
+
+	private void PlayRandomClipFromList(List<AudioSource> list)
+	{
+		list[ Random.Range( 0 , list.Count ) ].Play();
+	}
+
+
+}
